7c8f51625cfc268e1cf8f75a4ef39698bfc506f4
Sjors Snoeren <mail@sjorssnoeren.com>
2017-08-18 11:46:10 +0200

    Adds correct section padding for scroll anchors

--- assets/js/components/Section.jsx
+++ assets/js/components/Section.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  padding-top: 70px;
+  /* Negative margin added to make sure the fixed navigation will
+  not overlap with scrolling anchors */
+  margin-top: -70px;
+  /* Visible padding-top 70px */
+  padding-top: 140px;
 
   @media screen and (max-width: 40em) {
-    padding-top: 30px;
+    margin-top: -60px;
+    /* Visible padding-top 30px */
+    padding-top: 90px;
   }
 `;
 
